e779817097d6043198ba7c250079cb10c199a616
Md. Tanvir Ahmed <tanvirahmed.cse.bubt@gmail.com>
2020-01-11 00:54:23 +0600

    setState with two parameter an object and a call back function

--- firstreact/src/components/Counter.js
+++ firstreact/src/components/Counter.js
@@ -12,14 +12,15 @@ class Counter extends Component{
         this.setState(
             {
                 count: this.state.count + 1
-            }
+            },
+            () => console.log(this.state.count)
         )
     }
 
     render(){
         return(
             <div>
-                <h1>{this.state.count%2}</h1>
+                <h1>{this.state.count}</h1>
                 <button onClick={() => this.increment()}>Increment</button>
             </div>
         );
